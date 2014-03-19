9c19f6836e034f8f1e24f7be3d907db10804e3ce
David Carney <dfcarney@4ormat.com>
2014-03-18 23:31:25 -0400

    script to dump some Cassandra contents

---
+++ bin/dump_cassandra.rb
@@ -0,0 +1,14 @@
+require 'cql'
+require 'lib/debs/cassandra_helpers.rb'
+
+include CassandraHelpers
+
+query = "SELECT * from InstantaneousPlugLoads" # ORDER BY house_id, household_id, plug_id, timestamp" 
+puts "#{query}"
+puts store.execute(query).map{|row| row}
+
+query = "SELECT * from AveragePlugLoads"
+puts "\n#{query}"
+puts store.execute(query).map{|row| row}
+
+
