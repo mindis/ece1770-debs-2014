6daaa5fe82542645a149ed77316e20330e239b5d
David Carney <dfcarney@4ormat.com>
2014-03-19 12:19:51 -0400

    cleanup

--- lib/debs/debs_plug_bolt.rb
+++ lib/debs/debs_plug_bolt.rb
@@ -23,8 +23,9 @@ class DebsPlugBolt < RedStorm::DSL::Bolt
   on_receive :emit => false, :ack => false, :anchor => false do |tuple|
     @tuple = tuple
     if tuple_contains_load_value?
-      update_real_plug_load
-      datum = [timestamp, house_id, household_id, plug_id, predict_plug_load]
+      update_current_plug_load
+      predicted = predict_plug_load
+      datum = [timestamp, house_id, household_id, plug_id, predicted]
       anchored_emit(tuple, *datum)
     end
     ack(tuple)
