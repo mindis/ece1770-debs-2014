1d0da526127627b7516114e8c77b42299662bdf1
David Carney <dfcarney@4ormat.com>
2014-04-19 08:39:35 -0400

    Just dump the metrics.

--- bin/dump_cassandra.rb
+++ bin/dump_cassandra.rb
@@ -6,15 +6,18 @@ require 'lib/debs/cassandra_helpers.rb'
 include CassandraHelpers
 
 query = "SELECT * from InstantaneousPlugLoads" # ORDER BY house_id, household_id, plug_id, timestamp" 
-puts "#{query}"
-puts store.execute(query).map{|row| row}
+#puts "#{query}"
+#puts store.execute(query).map{|row| row}
 
 query = "SELECT * from AveragePlugLoads"
-puts "\n#{query}"
-puts store.execute(query).map{|row| row}
+#puts "\n#{query}"
+#puts store.execute(query).map{|row| row}
 
 query = "SELECT * from AverageHouseLoads"
+#puts "\n#{query}"
+#puts store.execute(query).map{|row| row}
+
+query = "SELECT * from Metrics"
 puts "\n#{query}"
 puts store.execute(query).map{|row| row}
-
 
