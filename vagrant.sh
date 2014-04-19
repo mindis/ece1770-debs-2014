e8c495ca7e10a26609721a0f627d936e34222188
David Carney <dfcarney@4ormat.com>
2014-04-19 08:40:25 -0400

    script for deploying to vagrant cluster

---
+++ vagrant.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./bin/setup_cassandra.rb
+
+cp ~/.storm/storm.yaml ./target/
+#bundle exec redstorm build --1.6
+cp ./lib/debs/*helpers*.rb ./target/lib/debs/
+bundle exec redstorm jar lib 
+bundle exec redstorm cluster lib/debs/debs_topology.rb 
