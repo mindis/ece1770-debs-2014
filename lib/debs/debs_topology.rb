2dc81ac8e6b9143839921201fb17dc88b512a32f
David Carney <dfcarney@4ormat.com>
2014-04-19 21:36:21 -0400

    test and notes for eventual EC2 deploy Conflicts: 	lib/debs/debs_topology.rb

--- lib/debs/debs_topology.rb
+++ lib/debs/debs_topology.rb
@@ -36,25 +36,25 @@ class KafkaTopology < RedStorm::DSL::Topology
     max_task_parallelism 1
   end
 
-  bolt DebsPlugBolt, :parallelism => 2 do
+  bolt DebsPlugBolt, :parallelism => 8 do
     output_fields :id, :timestamp, :value, :property, :plug_id, :household_id, :house_id, :start_time
     source DebsDataBolt, :fields => [:house_id, :household_id, :plug_id]
     max_task_parallelism 8
   end
 
-  bolt DebsPlugBolt2, :parallelism => 2 do
+  bolt DebsPlugBolt2, :parallelism => 8 do
     output_fields :id, :timestamp, :house_id, :household_id, :plug_id, :predicted_plug_load, :start_time
     source DebsPlugBolt, :fields => [:house_id, :household_id, :plug_id]
     max_task_parallelism 8
   end
 
-  bolt DebsHouseCalcBolt, :parallelism => 2 do
+  bolt DebsHouseCalcBolt, :parallelism => 8 do
     output_fields :id, :timestamp, :house_id, :household_id, :plug_id, :predicted_plug_load, :start_time
     source DebsPlugBolt2, :fields => [:house_id]
     max_task_parallelism 8
   end
 
-  bolt DebsHouseCalcBolt2, :parallelism => 2 do
+  bolt DebsHouseCalcBolt2, :parallelism => 8 do
     output_fields :id, :timestamp, :house_id, :predicted_house_load, :start_time
     source DebsHouseCalcBolt, :fields => [:house_id]
     max_task_parallelism 8
@@ -67,9 +67,17 @@ class KafkaTopology < RedStorm::DSL::Topology
   end
 
   configure do |env|
+
+    # NOTE: machines (4) -> num_workers (x2 = 8) -> executors (parallalism hint) -> tasks (executors)
+    # The number of workers and executors can be "rebalanced" (i.e. only reduced), but not the number of tasks.
+    # The 'parallelism' hint above specifies the initial number of executors (and tasks). It's 1:1 by default.
+
+    # redstorm creates 4 worker 'slots' for each machine by default
+    # The config below specifies how many should be used by this topology.
+
     debug false
-    max_task_parallelism 16
-    num_workers 8
+    max_task_parallelism 8
+    num_workers 8 # <-- how many slots to use. up to 4 per machine, but keep it at 2 (1 per CPU on m1.large)
     max_spout_pending 10000 # 16000
   end
 
